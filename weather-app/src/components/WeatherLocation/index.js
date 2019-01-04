875a37fee77b006116b6792693a409d9ba27216c
darrubla <darrubla@uninorte.edu.co>
2019-01-04 16:05:53 -0500

    componente weatherdata y weatherlocation completos

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -3,10 +3,16 @@ import Location from './Location.js';
 import WeatherData from './WeatherData';
 import './styles.css';
 
+const data = {
+    temperature: 5,
+    humidity: 10,
+    wind: '15m/s',
+}
+
 const WeatherLocation = () => (
     <div className="weather-location-cont">
         <Location city={"Barranquilla"}></Location>
-        <WeatherData></WeatherData>
+        <WeatherData data={data}></WeatherData>
     </div>
 );
 
